986600c7e2e01ca7cfd4bb7f241b6fa2f8f6076e
lennytheworm12 <153835209+lennytheworm12@users.noreply.github.com>
2026-08-14 19:28:04 -0700

    Add: products-of-array-discluding-self - submission-6

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-6.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        vector<int> suffix(nums.size());
+        int prefix = 1;
+
+        for (int i = nums.size() -1; i >= 0 ; i--) {
+            if (i == nums.size()-1) {
+                suffix[i] = 1;
+
+            }
+            else {
+                suffix[i] = suffix[i+1] * nums[i+1];
+            }
+        }
+
+        for (int i =0; i < nums.size(); i++ ){
+            suffix[i] = prefix * suffix[i];
+            prefix = prefix * nums[i];
+
+
+        }
+        return suffix;
+
+
+
+    }
+};
+
+
