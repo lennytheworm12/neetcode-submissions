5da1562799a0ab73154e6d2546da421ddcfb3604
lennytheworm12 <153835209+lennytheworm12@users.noreply.github.com>
2026-08-21 19:30:46 -0700

    Add: two-integer-sum-ii - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-4.cpp
@@ -0,0 +1,25 @@
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        vector<int> ans(2);
+
+        int l =0, r = numbers.size()-1;
+
+        while (l < r) {
+            int sum = numbers[l] + numbers[r];
+            if (target < sum) {
+                r--;
+            }
+            else if (target > sum) {
+                l++;
+            }
+            else {
+                return {l+1, r+1};
+            }
+        }
+    
+        return ans;
+    }
+};
+
